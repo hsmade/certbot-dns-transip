55646e143c15f9ec87cf30971c1ef06e5ab1dff7
Wim Fournier <wim@fournier.nl>
2017-07-15 00:21:16 +0200

    refactor travis-ci

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        '''License :: OSI Approved :: Apache license 2.0 License''',
+        '''License :: OSI Approved :: Apache Software License''',
         'Natural Language :: English',
         'Programming Language :: Python',
     ],
